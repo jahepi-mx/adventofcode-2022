15e4dbd1e9ae448b418aca3c4f7c80d998def389
Jahepi <53530187+jahepi-mx@users.noreply.github.com>
2022-12-09 17:34:39 -0600

    Update Day9.java

--- src/adventofcode2022/Day9.java
+++ src/adventofcode2022/Day9.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 
 public class Day9 {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         ArrayList<String> list = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader("input/day9/input.txt"));
         String line = "";
@@ -20,31 +20,31 @@ public class Day9 {
         System.out.println("Part 1: " + day9.run(list, 2));
         System.out.println("Part 2: " + day9.run(list, 10));
     }
-	
+
     private int run(ArrayList<String> list, int size) {
-    	HashSet<Integer> set = new HashSet<Integer>();
-    	set.add(0);
-    	int[][] rope = new int[size][2];
-    	for (String line : list) {
-    		int move = Integer.valueOf(line.split(" ")[1]);
-    		while (move-- > 0) {
-    			rope[0][0] += line.charAt(0) == 'R' ? 1 : 0;
-    			rope[0][0] += line.charAt(0) == 'L' ? -1 : 0;
-    			rope[0][1] += line.charAt(0) == 'U' ? 1 : 0;
-    			rope[0][1] += line.charAt(0) == 'D' ? -1 : 0;
-    			for (int a = 0; a < rope.length - 1; a++) {
-		    		int subX = rope[a][0] - rope[a + 1][0];
-		    		int subY = rope[a][1] - rope[a + 1][1];
-		    		if (subX * subX + subY * subY >= 4) {
-		    			subX = subX != 0 ? subX / Math.abs(subX) : subX;
-		    			subY = subY != 0 ? subY / Math.abs(subY) : subY;
-		    			rope[a + 1][0] += subX;
-		    			rope[a + 1][1] += subY;
-		    		}
-    			}
-    			set.add(rope[size - 1][1] * 1000 + rope[size - 1][0]);
-    		}
-    	}
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(0);
+        int[][] rope = new int[size][2];
+        for (String line : list) {
+            int move = Integer.valueOf(line.split(" ")[1]);
+            while (move-- > 0) {
+                rope[0][0] += line.charAt(0) == 'R' ? 1 : 0;
+                rope[0][0] += line.charAt(0) == 'L' ? -1 : 0;
+                rope[0][1] += line.charAt(0) == 'U' ? 1 : 0;
+                rope[0][1] += line.charAt(0) == 'D' ? -1 : 0;
+                for (int a = 0; a < rope.length - 1; a++) {
+                    int subX = rope[a][0] - rope[a + 1][0];
+                    int subY = rope[a][1] - rope[a + 1][1];
+                    if (subX * subX + subY * subY >= 4) {
+                        subX = subX != 0 ? subX / Math.abs(subX) : subX;
+                        subY = subY != 0 ? subY / Math.abs(subY) : subY;
+                        rope[a + 1][0] += subX;
+                        rope[a + 1][1] += subY;
+                    }
+                }
+                set.add(rope[size - 1][1] * 1000 + rope[size - 1][0]);
+            }
+        }
         return set.size();
     }
 }
